60f7cc39719f0c68066bba09838708536ab30d63
risma30 <78523932+risma30@users.noreply.github.com>
2021-03-20 20:41:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,21 @@ app.use("/pegawai", auth, pegawai)
 const siswa = require("./route/siswa")
 app.use("/siswa",   siswa)
 
+//import route jurusan
+const jurusan = require("./route/jurusan")
+app.use("/jurusan",  auth, jurusan)
 
 //import route pelanggaran
 const pelanggaran = require("./route/pelanggaran")
 app.use("/pelanggaran", auth, pelanggaran)
+
+//base url -> http://localhost:2000/pelanggaran_siswa
+const pelanggaran_siswa = require("./route/pelanggaran_siswa")
+app.use("/pelanggaran_siswa", auth, pelanggaran_siswa)
+
+//base url -> http://localhost:2000/detail_pelanggaran_siswa
+const detail_pelanggaran_siswa = require("./route/detail_pelanggaran_siswa")
+app.use("/detail_pelanggaran_siswa", auth, detail_pelanggaran_siswa)
 
 //membuat web server dengan port 2000
 app.listen(2000, () => {
